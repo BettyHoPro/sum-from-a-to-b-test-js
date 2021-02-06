12e0af6db500126396e1996d479e21dc0c657390
BettyHoPro <bettyvisualtech@gmail.com>
2021-02-06 20:13:05 +0000

    Submit the god bless me confusing recursion stretch

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
-}
+  if (fromN < toN) {
+    return fromN + sum(fromN + 1, toN);
+  }
+  return fromN;
+};
+console.log(sum(5,3));
+
+// function sum(fromN, toN) {
+//   let sumN = 0;
+//   if (fromN === toN) {
+//     return fromN;
+//   }
+//   if (fromN < toN) {
+//      sumN += sum(fromN + 1);
+//   }
+//   return sumN
+// };
+//console.log(sum(fromN, toN));
+sum(3,7);
+//console.log(sum(3, 7));
 
 module.exports = sum;
+
+// const sumToOne = (n) => {
+//   if (n === 1) {
+//     return 1;
+//   }
+//   return n + sumToOne(n - 1);
+// };
+
+// console.log(sumToOne(4));
